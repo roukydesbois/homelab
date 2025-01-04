31941830a9da87022a64ada9ad408f5906f21bb1
Thomas Blarre <thomas@blarre.net>
2025-01-04 17:17:39 +0000

    argocd app of apps module

---
+++ opentofu/modules/argocd-homelab-project/main.tf
@@ -0,0 +1,60 @@
+resource "kubernetes_manifest" "app_project_namespace" {
+  manifest = {
+    apiVersion = "v1"
+    kind       = "Namespace"
+    metadata = {
+      name = var.app_project_namespace
+    }
+  }
+}
+
+resource "kubernetes_manifest" "app_project" {
+  depends_on = [ kubernetes_manifest.app_project_namespace ]
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "AppProject"
+    metadata = {
+      name = var.app_project_name
+      namespace = var.argocd_namespace
+    }
+    spec = {
+      destination = {
+        namespace = var.app_project_namespace
+        server = "https://kubernetes.default.svc"
+      }
+      sourceRepos = [
+        var.app_project_source_repos
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "argocd_app_of_apps" {
+  depends_on = [ kubernetes_manifest.app_project ]
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name = "argocd-app-of-apps"
+      namespace = var.argocd_namespace
+    }
+    spec = {
+      project = var.app_project_name
+      source = {
+        for_each = var.app_of_apps_source_config
+        key = each.key
+        value = each.value
+      }
+      destination = {
+        namespace = var.app_project_namespace
+        server = "https://kubernetes.default.svc"
+      }
+      syncPolicy = {
+        automated = {
+          prune = true
+          selfHeal = true
+        }
+      }
+    }
+  }
+}
---
+++ opentofu/modules/argocd-homelab-project/variables.tf
@@ -0,0 +1,25 @@
+variable "argocd_namespace" {
+  description = "The namespace ArgoCD is deployed in"
+  type        = string
+  default = "argocd"
+}
+
+variable "app_project_name" {
+  description = "The name of the ArgoCD project"
+  type        = string
+}
+
+variable "app_project_namespace" {
+  description = "The namespace to deploy the ArgoCD project in"
+  type        = string
+}
+
+variable "app_project_source_repos" {
+  description = "The source repositories to sync with the ArgoCD project"
+  type        = list(string)
+}
+
+variable "app_of_apps_source_config" {
+  description = "The configuration for the App of Apps"
+  type        = map(any)
+}
